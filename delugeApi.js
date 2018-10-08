bdf09d47e9ab49dac7fca2fed4804aff1a04a0f0
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2018-10-08 18:39:21 -0400

    merged delugeApi

--- delugeApi.js
+++ delugeApi.js
@@ -3,9 +3,9 @@
 const exec = require('child_process').exec;
 
 let COMMANDS = {
-	'INFO': `deluge-console 'info -i'`,
-	//'ADD': `deluge-console "add '${magnetLink}'"`,
-	//'RM': `deluge-console "rm '${torrentId}'"`
+	'INFO': `deluge-console 'info'`
+	// 'ADD': `deluge-console "add '${magnetLink}'"`,
+	// 'RM': `deluge-console "rm '${torrentId}'"`
 };
 
 async function delugeInfo(){
@@ -24,7 +24,7 @@ async function delugeInfo(){
 async function getCompletedTorrents(){
 	try {
 		let infos = await delugeInfo();
-		let torrents = parseDelugeInfo();
+		let torrents = parseDelugeInfo(infos);
 		return torrents;
 
 	}
@@ -35,10 +35,46 @@ async function getCompletedTorrents(){
 };
 
 function parseDelugeInfo(infoString){
-}	
-function TorrentState(name, id, state, size) {
+
+	let lines = infoString.split('\n');
+	let torrents = [];
+	let i = 0;
+
+	while (i < lines.length){
+		//check all the if's to make sure it only adds if all reqs are satisfied
+		if (lines[i].startsWith('Name: ') && 
+			lines[i+1].startsWith('ID: ') &&
+			lines[i+2].startsWith('State: ') &&
+			lines[i+4].startsWith('Size: ')){	//size is completion
+
+			let name = lines[i].substring(6),
+				id = lines[i+1].substring(4),
+				state = lines[i+2].substring(7),
+				completion = lines[i+4].substring(6);
+
+			torrents.push(new TorrentState(name, id, state, completion));
+
+			//increment by 5 to skip the things we used
+			i += 5;
+		}
+		else {
+			++i;
+		}
+	}
+
+	return torrents;
 }
 
+function TorrentState(name, id, state, completion) {
+	this.name = name;
+	this.id = id;
+	this.state = state;
+
+	//get size and completeness
+	let temp = completion.split('/');
+	this.size = temp[0];
+	this.isCompleted = temp[1].startsWith(this.size);
+}
 
 module.exports = {
 	getCompletedTorrents: getCompletedTorrents
--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 "use strict";
 
 const horribleApi = require('./horribleApi');
+const delugeApi = require('./delugeApi');
+
 
 async function main(){
-	let shows = await horribleApi.getCurrentShows();
-	let magnets = await horribleApi.getFilteredMagnets(["One Piece"], "720p");
+	// let shows = await horribleApi.getCurrentShows();
+	// let magnets = await horribleApi.getFilteredMagnets(["One Piece"], "720p");
 
-	console.log(JSON.stringify(shows));
-	console.log(JSON.stringify(magnets));
+	// console.log(JSON.stringify(shows));
+	// console.log(JSON.stringify(magnets));
+
+	let infoString = 
+`Name: [HorribleSubs] Sword Art Online - Alicization - 01 [720p].mkv
+ID: c6e1336d7f24ee440c5273b7c8e2e2c00b2b990a
+State: Queued
+Seeds: 0 (1875) Peers: 0 (100) Availability: 0.00
+Size: 945.8 MiB/945.8 MiB Ratio: 0.000
+Seed time: 0 days 00:01:09 Active: 0 days 00:11:31
+Tracker status: tracker.wf: Announce OK
+Name: [HorribleSubs] One Piece - 856 [720p].mkv
+ID: 89c0e9d1675f185d9f1f698beaf3ef1e0a47523b
+State: Seeding Up Speed: 474.0 KiB/s
+Seeds: 0 (916) Peers: 4 (43) Availability: 0.00
+Size: 485.3 MiB/485.3 MiB Ratio: 0.025
+Seed time: 0 days 00:06:11 Active: 0 days 00:08:59
+Tracker status: tracker.wf: Announce OK
+Name: [HorribleSubs] Sword Art Online - Alicization - 01 [720p].mkv
+ID: c6e1336d7f24ee440c5273b7c8e2e2c00b2b990a
+State: Downloading Down Speed: 3.1 MiB/s Up Speed: 1.2 KiB/s ETA: 10s
+Seeds: 192 (1899) Peers: 3 (101) Availability: 191.99
+Size: 913.7 MiB/945.8 MiB Ratio: 0.000
+Seed time: 0 days 00:00:00 Active: 0 days 00:05:00
+Tracker status: tracker.wf: Announce OK
+Progress: 96.61% [#########################################################~~]`;
+	let infos = await delugeApi.parse(infoString);
+	console.log(JSON.stringify(infos, 0, 2));
 }
 
 main();
